fcaf8b027587be90a2c0c783d7360d542b462649
Angelo <agosromeo@gmail.com>
2025-05-20 11:52:57 +0200

    edited huburl v3

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -32,7 +32,7 @@ public class OptionsManager {
 			
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			co.setCapability("browserName", "chrome");
-			co.setBrowserVersion(prop.getProperty("browserversion").trim());
+			co.setBrowserVersion(prop.getProperty("browserVersion").trim());
 			
 			Map<String, Object> selenoidOptions = new HashMap<>();
 			selenoidOptions.put("screenResolution","1280x1024x24");
@@ -59,7 +59,7 @@ public class OptionsManager {
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			fo.setCapability("browserName", "firefox");
-			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+			fo.setBrowserVersion(prop.getProperty("browserVersion").trim());
 			
 			Map<String, Object> selenoidOptions = new HashMap<>();
 			selenoidOptions.put("screenResolution","1280x1024x24");
@@ -86,7 +86,7 @@ public class OptionsManager {
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			eo.setCapability("browserName", "edge");
-			eo.setBrowserVersion(prop.getProperty("browserversion").trim());
+			eo.setBrowserVersion(prop.getProperty("browserVersion").trim());
 			
 			Map<String, Object> selenoidOptions = new HashMap<>();
 			selenoidOptions.put("screenResolution","1280x1024x24");
